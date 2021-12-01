52042fdb8c0775a0f423a6dfb44de62bcc540768
Williams OZSERTTAS <wiozsert@e3r4p23.clusters.42paris.fr>
2021-12-01 10:04:45 +0100

    Create Arbo

---
+++ inc/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 09:42:09 by wiozsert          #+#    #+#             */
+/*   Updated: 2021/12/01 10:04:27 by wiozsert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "./minishell_structures.h"
+# include "./minishell_headers.h"
+
+#endif
