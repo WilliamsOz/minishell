156d9d82eb4de704507ebb86de50e6b01046f0c3
Oscar OZSERTTAS <oozsertt@e2r13p12.clusters.42paris.fr>
2022-01-02 18:08:11 +0100

    save

--- srcs/builtins/execute_builtin.c
+++ srcs/builtins/execute_builtin.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   which_builtin.c                                    :+:      :+:    :+:   */
+/*   execute_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wiozsert <wiozsert@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 16:32:44 by wiozsert          #+#    #+#             */
-/*   Updated: 2022/01/02 16:35:31 by wiozsert         ###   ########.fr       */
+/*   Updated: 2022/01/02 16:39:37 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	execute_builtin(t_minishell *m, t_cmd *tmp_cmd)
 {
-	if (ft_strcmp("cd", str) == TRUE)
-		cd_builtin(tmp_cmd, m->env);
-	if (ft_strcmp("echo", str) == TRUE)
+	if (ft_strcmp("cd", tmp_cmd->cmd[0]) == TRUE)
+		cd_builtin(tmp_cmd, &m->env);
+	if (ft_strcmp("echo", tmp_cmd->cmd[0]) == TRUE)
 		echo_builtin(tmp_cmd);
-	if (ft_strcmp("env", str) == TRUE)
+	if (ft_strcmp("env", tmp_cmd->cmd[0]) == TRUE)
 		env_builtin(m->env);
-	if (ft_strcmp("exit", str) == TRUE)
+	if (ft_strcmp("exit", tmp_cmd->cmd[0]) == TRUE)
 		exit_builtin(tmp_cmd);
-	if (ft_strcmp("export", str) == TRUE)
-		export_builtin(tmp_cmd, m->env);
-	if (ft_strcmp("pwd", str) == TRUE)
+	if (ft_strcmp("export", tmp_cmd->cmd[0]) == TRUE)
+		export_builtin(tmp_cmd, &m->env);
+	if (ft_strcmp("pwd", tmp_cmd->cmd[0]) == TRUE)
 		pwd_builtin(tmp_cmd, m->env);
-	if (ft_strcmp("unset", str) == TRUE)
-		unset_builtin(tmp_cmd, m->env);
+	if (ft_strcmp("unset", tmp_cmd->cmd[0]) == TRUE)
+		unset_builtin(tmp_cmd, &m->env);
 }
