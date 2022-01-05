71fe921b1e25b299a031a1ff38ef17cd117e9156
Oscar OZSERTTAS <oozsertt@e2r13p8.clusters.42paris.fr>
2022-01-05 14:54:19 +0100

    update

--- srcs/builtins/unset_builtin.c
+++ srcs/builtins/unset_builtin.c
@@ -6,11 +6,9 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 17:02:20 by oozsertt          #+#    #+#             */
-/*   Updated: 2022/01/05 13:23:48 by oozsertt         ###   ########.fr       */
+/*   Updated: 2022/01/05 14:52:36 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// si arg correspond a aucune variable d'env, ne fais rien
 
 #include "../../inc/minishell.h"
 
@@ -39,21 +37,38 @@ static void	unset_var(t_env *previous, t_env *node, t_env *next)
 	free(node);
 }
 
+static t_env	*unset_first_var(t_env *env)
+{
+	t_env	*tmp;
+
+	tmp = env;
+	tmp = tmp->next;
+	free(env->var);
+	free(env);
+	return (tmp);
+}
+
 void	unset_builtin(t_cmd *cmd, t_env **env)
 {
 	t_env	*tmp;
 	t_env	*previous;
+	int		i;
 
 	if (cmd->cmd[1] == NULL)
 		return ;
 	tmp = *env;
+	i = 0;
 	while (tmp != NULL)
 	{
 		if (is_var(tmp->var, cmd->cmd[1]) == 1)
 		{
-			unset_var(previous, tmp, tmp->next);
+			if (i == 0)
+				*env = unset_first_var(tmp);
+			else
+				unset_var(previous, tmp, tmp->next);
 			return ;
 		}
+		i++;
 		previous = tmp;
 		tmp = tmp->next;
 	}	
